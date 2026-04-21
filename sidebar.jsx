2bd8b1153853d20f2616648c83d15b37a7b5a7ec
Gracee001-M <gracemacharia031@gmail.com>
2026-04-21 13:55:40 +0000

    Add sidebar.jsx

---
+++ sidebar.jsx
@@ -0,0 +1,15 @@
+// src/components/Sidebar.jsx
+function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <h3>Sidebar</h3>
+      <ul>
+        <li><a href="#">Latest Posts</a></li>
+        <li><a href="#">Categories</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </aside>
+  );
+}
+
+export default Sidebar;
